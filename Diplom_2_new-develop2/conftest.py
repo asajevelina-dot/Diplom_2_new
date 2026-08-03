82f4652a7259f6b3ac9ff114d33c59cde0faa2cd
Evelina Asay <_e.asai@yandex.ru>
2026-08-03 19:54:37 +0300

    Задание 2: API-тесты с Allure

--- Diplom_2_new-develop2/conftest.py
+++ Diplom_2_new-develop2/conftest.py
@@ -7,7 +7,7 @@ from data.test_data import USER_NAMES, PASSWORDS
 
 @pytest.fixture
 def create_and_delete_user():
-    """Создаёт пользователя и удаляет его после теста"""
+    """Фикстура для тестов логина (создаёт и удаляет пользователя)"""
     email = f"test_user_{int(time.time())}@yandex.ru"
     payload = {
         "email": email,
